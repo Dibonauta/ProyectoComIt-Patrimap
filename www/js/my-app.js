0e4e38d07a99ee7a099c13b19ff3f76d1e37e69a
Dibonauta <santidibono@gmail.com>
2021-10-26 16:37:29 -0300

    Error en consola

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,7 +1,8 @@
   
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
-const platform = new H.service.Platform({'apikey': '{jxHbNasTU-AJ9dPFrehNs5dC7JtDW7hz-KjaiKcSb-0}'});
+var map, platform;
+var pos, latitud, longitud;
 var app = new Framework7({
     // App root element
     root: '#app',
@@ -27,7 +28,10 @@ var mainView = app.views.create('.view-main');
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+    console.log("Device is ready!"); 
+    platform = new H.service.Platform({
+      'apikey': '{jxHbNasTU-AJ9dPFrehNs5dC7JtDW7hz-KjaiKcSb-0}'
+    });
 });
 
 // Option 1. Using one 'page:init' handler for all pages
@@ -39,4 +43,23 @@ $$(document).on('page:init', function (e) {
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
     // Do something here when page with data-name="index" attribute loaded and initialized
+     
+
+    var defaultLayers = platform.createDefaultLayers();
+	    // Instantiate (and display) a map object:
+	    map = new H.Map(
+          document.getElementById('mapContainer'),
+    	  defaultLayers.vector.normal.map,
+    	  {
+      	  zoom: 14,
+      	  center: { lat: latitud, lng: longitud }
+          });
+ 
+    	  coords = {lat: latitud, lng: longitud};
+    	  marker = new H.map.Marker(coords);
+ 
+    	  // Add the marker to the map and center the map at the location of the marker:
+    	  map.addObject(marker);
+      	map.setCenter(coords);
+
 })
