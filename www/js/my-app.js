2775dba7dbac61d5b66de15b0e4e53d930882751
Dibonauta <santidibono@gmail.com>
2021-11-16 14:51:30 -0300

    En Una Lejana Playa Del Animus

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -20,7 +20,8 @@ var app = new Framework7({
     routes: [
       {path: '/index/', url: 'index.html',},
       {path: '/registro/', url: 'registro.html',},
-      {path: '/login/', url: 'login.html',},
+      {path: '/login/', url: 'login.html',}, 
+      {path: '/perfil/', url: 'perfil.html',},
     ]
     // ... other parameters
   });
@@ -28,7 +29,8 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 
 var db = firebase.firestore();
-var coleccionUsuarios = db.collection("USUARIOS");
+var coleccionUsuarios = db.collection("USUARIOS"); 
+
 
 
 // Handle Cordova Device Ready Event
@@ -81,6 +83,14 @@ $$(document).on('page:init', '.page[data-name="login"]', function (e) {
   // Do something here when page with data-name="index" attribute loaded and initialized
     $$('#ingresar').on('click', fnIngresoUsuario); 
     $$('#ingresar').on('click', fnApareceEsto);
+    $$('#ingresar').on('click', fnAu);
+    $$('#ingresar').on('click', fnAr);
+    $$('#ingresar').on('click', fnAl);
+})  
+
+$$(document).on('page:init', '.page[data-name="perfil"]', function (e) {
+  // Do something here when page with data-name="index" attribute loaded and initialized
+  fnPerfilUsuario()
 }) 
 
 function fnPuntosMapa(){
@@ -125,7 +135,8 @@ function fnPuntosMapa(){
 //---------------------------------------------------iconos del mapa---------------------------------------------------------
         /*var icon = new H.map.Icon('img/icono1.png')*/ 
         var iconoMuseo = new H.map.Icon('img/iconomuseo.png')
-        var iconoPn = new H.map.Icon('img/iconopn.png')
+        var iconoPn = new H.map.Icon('img/iconopn.png') 
+        var iconoPa = new H.map.Icon('img/iconopa.png')
         //var marker = new H.map.Marker({ lat: -32.845811, lng: -61.248997 }, {icon: icon});
         
         //map.addObject(marker); 
@@ -268,20 +279,68 @@ function fnPuntosMapa(){
        
 
 
+        //Patrimonios
+        function marcadores2(map){
+
+          var grupopa = new H.map.Group(); 
+
+          map.addObject(grupopa);  
+
+          grupopa.addEventListener('tap', function (evt) {
+            // event target is the marker itself, group is a parent event target
+            console.log(evt.target.getData())
+            // for all objects that it contains
+            var bubble = new H.ui.InfoBubble(evt.target.getGeometry(), {
+              // read custom data 
+
+              content: evt.target.getData() 
+            });
+            // show info bubble
+            ui.addBubble(bubble);
+          }, false);        
+          
+          //Parques Nacionales
+            var db = firebase.firestore();
+            var perRef = db.collection("Patrimonios Culturales");
+            perRef.get()
+              .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                console.log("Enlace:" + doc.data().Link);
+                console.log("Latitud:" + doc.data().Latitud); 
+                console.log("Longitud:" + doc.data().Longitud); 
+                console.log("Nombre:" + doc.data().Nombre);
+                console.log("Ubicacion:" + doc.data().Ubicacion); 
+
+                enlace = doc.data().Link;
+                ns = doc.data().Latitud; 
+                eo = doc.data().Longitud; 
+                nom = doc.data().Nombre;   
+                ubi = doc.data().Ubicacion; 
+
+                agregarpa(grupopa, {lat: ns, lng: eo},
+                  '<div><a class="external" target="_blank" href="'+ enlace +'">'+ nom +'</a></div>' +
+                  '<div>'+ ubi +'</div>' + '<h3 onclick="abrirpopupa(\''+doc.id+'\')" style="cursor: pointer;">Mas Info</h3>'); 
+               
+                }); 
+            
+              })
+              .catch(function(error) {
+
+               console.log("Error: " , error); 
+              });
+         
+        } 
+
+        function agregarpa(grupopa, coordinate, html){
+          var marcadorPa = new H.map.Marker(coordinate, {icon: iconoPa}); 
+          grupopa.addObject(marcadorPa); 
+          marcadorPa.setData(html);
+        }  
+        marcadores2(map);
+       
 
 
-}
-
-
-
-
-
-
-
-
-
-
-
+} 
 
 //-----------------------------------------------------------POPUPS----------------------------------------------------------------------//
 
@@ -414,6 +473,67 @@ function abrirpopupn(id){
           console.log("Error getting document:", error);
           });
 
+} 
+
+
+
+
+
+//Patrimonios
+function abrirpopupa(id){
+  console.log(id) 
+
+  var pic = ""
+  var desc = ""
+
+
+
+  var db = firebase.firestore();
+  var docRef = db.collection("Patrimonios Culturales").doc(id);    
+  
+        docRef.get().then((doc) => {
+        if (doc.exists) {
+        console.log("Document data:", doc.data());
+        
+        console.log("Fotos:" + doc.data().Foto1);
+        console.log("Fotos:" + doc.data().Foto2);
+        console.log("Fotos:" + doc.data().Foto3);
+        console.log("Fotos:" + doc.data().Foto4);
+        console.log("Fotos:" + doc.data().Foto5);
+        
+        
+        console.log("Descripcion:" + doc.data().Descripcion);
+    
+          
+                  pic1 = doc.data().Foto1;
+                  pic2 = doc.data().Foto2;
+                  pic3 = doc.data().Foto3;
+                  pic4 = doc.data().Foto4;
+                  pic5 = doc.data().Foto5; 
+                  desc = doc.data().Descripcion; 
+                    
+                  imagen1 = '<center><img src="'+pic1+'" height="250" alt=""></center>'
+                  imagen2 = '<center><img src="'+pic2+'" height="250" alt=""></center>'
+                  imagen3 = '<center><img src="'+pic3+'" height="250" alt=""></center>'
+                  imagen4 = '<center><img src="'+pic4+'" height="250" alt=""></center>'
+                  imagen5 = '<center><img src="'+pic5+'" height="250" alt=""></center>'
+
+                  $$('#popuptitulo').html(id); 
+                  $$('#foto1').html(imagen1); 
+                  $$('#foto2').html(imagen2);
+                  $$('#foto3').html(imagen3);
+                  $$('#foto4').html(imagen4);
+                  $$('#foto5').html(imagen5);
+                  $$('#descripcion').html(desc)
+                  $$('#popupinvisible').click();
+        } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        }
+      }).catch((error) => {
+          console.log("Error getting document:", error);
+          });
+
 }
 
 //----------------------------------------------------REGISTRO-------------------------------------------------------------------
@@ -491,7 +611,7 @@ function fnIngresoUsuario() {
 
                     if (tipoUsuario == "Usuario") {
                         console.log("ya te logueaste");
-                         mainView.router.navigate('/index/');
+                         mainView.router.navigate('/perfil/');
                     } else {
                         console.log("admin B)");
                     }
@@ -529,12 +649,8 @@ function fnIngresoUsuario() {
 //-----------------------------------------------------------algo flota en la laguna------------------------------------------------------------------------
 
 function fnApareceEsto(){
-  if ( $$('#btf').hasClass('vr') ) {
     $$('#btf').removeClass('vr').addClass('seve');
-  } else {
-    $$('#btf').removeClass('seve').addClass('vr');
   }
-}
 
 
 
@@ -545,7 +661,67 @@ function fnAgregaFav(){
  } else {
    $$('#btf').removeClass('button-fill').addClass('button-outline').html('🤍');
  }
-}  
+}   
+
+
+
+//--------------------------------------------------------Perfil---------------------------------------------------------------------------
+
+function fnAr(){
+  $$('#regs').removeClass('seve').addClass('vr');
+}
+
+
+function fnAl(){
+  $$('#logs').removeClass('seve').addClass('vr');
+}
+
+
+function fnAu(){
+  $$('#pp').removeClass('vr').addClass('seve');
+}
+
+
+
+function fnPerfilUsuario(){
+  var docRef = db.collection("USUARIOS").doc(email);
+  docRef.get().then((doc) => {
+  if (doc.exists) {
+      console.log("Document data:", doc.data());
+      console.log("Nombre:" + doc.data().Nombre);
+      console.log("Apellido:" + doc.data().Apellido);
+      console.log("Email:" + doc.data().Email);
+      
+      
+      $$('#nomU').html("Nombre:" + "   " + doc.data().Nombre)
+      $$('#apellidoU').html("Apellido:" + "   " + doc.data().Apellido)
+      $$('#emailU').html("Email:" + "   " + doc.data().Email)
+      
+  } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+  }
+  }).catch((error) => {
+  console.log("Error getting document:", error);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
